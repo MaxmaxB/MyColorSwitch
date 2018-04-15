e906a7179399b5dd150f598affc98df640e00564
mbourdin <maxence.bourdin@capgemini.com>
2018-04-15 17:16:18 +0200

    Initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform player;
+
+    private void Update()
+    {
+        if(player.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
